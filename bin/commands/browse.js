becf1da415dd40b1cd9ab8e61406f6f3c1e9ff44
David Janes <davidjanes@davidjanes.com>
2014-12-31 12:57:06 -0500

    browse recipies

---
+++ bin/commands/browse.js
@@ -0,0 +1,45 @@
+/*
+ *  bin/commands/browse.js
+ *
+ *  David Janes
+ *  IOTDB.org
+ *  2014-12-30
+ *
+ *  HomeStar command line control: "homestar browse"
+ *  Start your local instance
+ *
+ *  Copyright [2013-2015] [David P. Janes]
+ *  
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *  
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *  
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+"use strict";
+
+var iotdb = require('iotdb');
+var _ = iotdb.helpers;
+var cfg = iotdb.cfg;
+
+var open = require('open');
+
+exports.command = "browse";
+exports.summary = "browse recipes"
+
+exports.help = function () {
+    console.log("usage: homestar browse");
+    console.log("");
+    console.log("Browse recipes");
+};
+
+exports.run = function (ad) {
+    open('https://github.com/dpjanes/homestar-cookbook/');
+};
