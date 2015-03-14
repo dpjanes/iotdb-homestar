ff3a330fe2c7e46d31f43b0f1a8217a392f3d75b
David Janes <davidjanes@davidjanes.com>
2015-03-14 08:01:54 -0400

    clean up click interaction

--- interactors/click/click.js
+++ interactors/click/click.js
@@ -3,7 +3,7 @@ js.interactors.click = {
 
     on_load: function() {
         $('li[data-interactor="click"]')
-            .on('click', js.interactors.click.on_click)
+            .each(js.interactors.click.setup_click)
             .on('touchstart', js.interactors.click.on_touchstart)
             .on('touchend', js.interactors.click.on_touchend)
             ;
@@ -17,17 +17,18 @@ js.interactors.click = {
         $(this).removeClass("touched");
     },
 
-    on_click: function(e) {
+    setup_click: function(e) {
         var e = $(this);
 
         var thing_id = e.data("thing");
         var attribute_code = e.data("attribute");
-
         var transporter = js.transport.connect(thing_id, "ostate");
+
         e.on("click", function() {
             var value = $(this).data("value");
             var thing_ostate = thingdd[thing_id]._ostate;
             thing_ostate[attribute_code] = (new Date()).toISOString();
+
             transporter.update(thing_ostate);
         });
     },
