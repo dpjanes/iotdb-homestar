3267f8a88c75d73067c0834279ec1ae05cd154a1
David Janes <davidjanes@davidjanes.com>
2016-05-12 07:25:37 -0400

    make friendly for developers

--- bin/commands/runner.js
+++ bin/commands/runner.js
@@ -46,12 +46,15 @@ exports.help = function () {
 exports.run = function (ad) {
     var node_path = process.execPath;
     var app_path = path.join("node_modules", "homestar", "app", "app.js");
-    // var app_path = path.join("app", "app.js");
-    var argv = [ app_path ].concat(process.argv.slice(3));
+    if (fs.stat(app_path, function(error) {
+        if (error) {
+            app_path = path.join("app", "app.js");
+        }
 
-    console.log(node_path, argv);
+        var argv = [ app_path ].concat(process.argv.slice(3));
 
-    child_process.spawn(node_path, argv, {
-        stdio: 'inherit'
-    });
+        child_process.spawn(node_path, argv, {
+            stdio: 'inherit'
+        });
+    }));
 };
