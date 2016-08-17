1ea34d7eae10771d61ac7415b80ce8aff766727c
David Janes <davidjanes@davidjanes.com>
2016-08-17 06:52:18 -0400

    scrub cookbooks and mqtt from dynamic

--- dynamic/cookbooks.js
+++
@@ -1,6 +0,0 @@
-var thingdd = {};
-{% for cookbook in cookbooks() %}
-    {% for recipe in cookbook.recipes %}
-thingdd[{{ recipe._id|json|safe }}] = {{ recipe|json(2)|safe }};
-    {% endfor %}
-{% endfor %}
